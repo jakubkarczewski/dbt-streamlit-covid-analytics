d635b688ecc8f0ae6df76a4f10da5c049718bac0
Jakub Karczewski <jakub.karczewski@nethone.com>
2022-12-10 21:28:46 +0100

    Reorganize app code

--- demo/app.py
+++ demo/app.py
@@ -6,70 +6,68 @@ import pandas as pd
 import streamlit as st
 from sqlalchemy import create_engine
 
-from get_data import COUNTRIES_DATA_PATH, get_country_data, COVID_DATA_PATH, prepare_template_file
+from get_data import COUNTRIES_DATA_PATH, COVID_DATA_PATH, prepare_template_file
+from utils.app_utils import populate_db
 
 engine = create_engine("postgresql://postgres:postgres@host.docker.internal:5432/postgres")
-
-
-def populate_db(countries):
-    with st.spinner("Creating the seeds..."):
-        for country in countries:
-            get_country_data(country)
-        time.sleep(1)
-    st.success("Seeds successfully created!")
-
-    with st.spinner("Installing dependencies..."):
-        os.system("dbt deps")
-        time.sleep(1)
-    st.success("Dependencies successfully installed!")
-
-    st.write("Populating db...")
-    with st.spinner("Inserting seeds..."):
-        os.system("dbt seed --profiles-dir ./profiles --target docker")
-        time.sleep(1)
-    st.success("Seeds successfully inserted!")
-
-    with st.spinner("Building models..."):
-        os.system("dbt run --full-refresh --profiles-dir ./profiles  --target docker")
-        time.sleep(1)
-    st.success("Models built!")
-
-    with st.spinner("Creating tests..."):
-        os.system("dbt test --profiles-dir ./profiles  --target docker")
-        time.sleep(1)
-    st.success("Tests created!")
-
-    time.sleep(0.5)
-
-    st.success("Db populated!")
-
-
 if not os.path.isfile(COVID_DATA_PATH):
     prepare_template_file()
-countries = pd.read_csv(COUNTRIES_DATA_PATH, index_col=0)["slug"].to_dict()
 
 st.write("# *DBT Visualisation App*")
-selected_countries = st.multiselect("Select countries: ", list(countries.keys()))
-selected_slugs = [countries.get(country) for country in selected_countries if countries.get(country) is not None]
+countries = pd.read_csv(COUNTRIES_DATA_PATH, index_col=0)["slug"].to_dict()
+selected_countries_for_display = st.multiselect("Select countries: ", list(countries.keys()))
+selected_countries = [
+    countries.get(country) for country in selected_countries_for_display if countries.get(country) is not None
+]
 
-col1, col2, col3, col4, col5, col6 = st.columns(6)
-with col1:
+(
+    get_data_col,
+    display_data_col,
+    show_table_col,
+    clear_col,
+) = st.columns(4)
+
+# TODO: learn how to do it properly in streamlit
+with get_data_col:
     get_data = st.button("Get Data", 1, "Get data for the countries selected")
-with col2:
-    clear = st.button("Clear", 2)
 
-if get_data:
-    populate_db(selected_slugs)
-    dataframe = pd.read_sql_table("covid_data", con=engine, schema="public_source")
-    st.write(dataframe)
-    chart = pd.read_sql_table("stg_deaths_per_month", con=engine)
-    alt_chart = (
-        alt.Chart(chart)
-        .mark_line()
-        .encode(
-            x=alt.X("month_year", title="Month"),
-            y=alt.Y("deaths", title="Number of Deaths"),
-            color=alt.Color("country", title="Countries"),
+with display_data_col:
+    display_data = st.button("Display Data", 2, "Display data for the countries selected in form of a chart")
+
+with show_table_col:
+    show_table = st.button("Show Table", 3)
+
+with clear_col:
+    clear = st.button("Clear", 4)
+
+# TODO: learn how handle clicks properly in streamlit
+if any((get_data, display_data, show_table)) and len(selected_countries) == 0:
+    st.write("Select countries first!")
+
+if get_data and len(selected_countries) > 0:
+    populate_db(selected_countries)
+
+if display_data and len(selected_countries) > 0:
+    with st.spinner("Loading data ..."):
+        covid_data = pd.read_sql_table("covid_data", con=engine, schema="public_source")
+        covid_data = covid_data[covid_data["country"].isin(selected_countries_for_display)]
+        time.sleep(1)
+
+    if len(covid_data) > 0:
+        st.success("Data Loaded!")
+        chart = pd.read_sql_table("stg_deaths_per_month", con=engine)
+        alt_chart = (
+            alt.Chart(chart)
+            .mark_line()
+            .encode(
+                x=alt.X("month_year", title="Month"),
+                y=alt.Y("deaths", title="Number of Deaths"),
+                color=alt.Color("country", title="Countries"),
+            )
         )
-    )
-    st.altair_chart(alt_chart, use_container_width=True)
+        st.altair_chart(alt_chart, use_container_width=True)
+
+if show_table and len(selected_countries) > 0:
+    covid_data = pd.read_sql_table("covid_data", con=engine, schema="public_source")
+    covid_data = covid_data[covid_data["country"].isin(selected_countries_for_display)]
+    st.write(covid_data)
--- demo/get_data.py
+++ demo/get_data.py
@@ -16,9 +16,7 @@ def add_population():
 
     if "population" not in countries_df.columns:
         countries_df["population"] = countries_df["iso2"].apply(
-            lambda code: pypopulation.get_population(code)
-            if isinstance(code, str)
-            else None
+            lambda code: pypopulation.get_population(code) if isinstance(code, str) else None
         )
         countries_df = countries_df.dropna(subset=["population"])
         countries_df.to_csv(COUNTRIES_DATA_PATH, index=False)
@@ -40,18 +38,14 @@ def add_daily_increments(country_df):
 def get_country_data(country):
     response = requests.get(f"https://api.covid19api.com/total/country/{country}")
     if response.status_code != 200:
-        raise Exception(
-            f"Failure in downloading data for {country}.\nReason: {response.reason}"
-        )
+        raise Exception(f"Failure in downloading data for {country}.\nReason: {response.reason}")
 
     country_df = pd.DataFrame(response.json())[RAW_COLUMNS]
     country_df.columns = [col.lower() for col in country_df.columns]
     country_df = add_daily_increments(country_df)
 
     all_available_countries_df = pd.read_csv(COVID_DATA_PATH)
-    all_available_countries_df = pd.concat(
-        [country_df, all_available_countries_df], axis=0
-    ).drop_duplicates()
+    all_available_countries_df = pd.concat([country_df, all_available_countries_df], axis=0).drop_duplicates()
 
     all_available_countries_df.to_csv(COVID_DATA_PATH, index=False)
 
